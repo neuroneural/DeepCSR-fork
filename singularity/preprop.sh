cd847547b002d58e7b456876cc684dccf2ce4b65
William A <wsashbee@gmail.com>
2022-07-01 12:49:10 -0400

    singualrity preproccesing works

---
+++ singularity/preprop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. /opt/miniconda3/bin/activate deepcsr
+export PYTHONPATH=/deepcsr/external/mesh_contains/
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+cd /deepcsr/
+
+#Read the string value
+
+# Set comma as delimiter
+IFS=' '
+
+#Read the split words into an array based on space delimiter
+read -a pmdata <<< "$1"
+
+#Print the splitted words
+
+python preprop.py outputs.output_dir=outdir \
+    inputs.sample_id=${pmdata[0]}  \
+    inputs.mri_vol_path=${pmdata[1]} \
+    inputs.lh_pial_path=${pmdata[2]} \
+    inputs.lh_white_path=${pmdata[3]} \
+    inputs.rh_pial_path=${pmdata[4]} \
+    inputs.rh_white_path=${pmdata[5]}
+
